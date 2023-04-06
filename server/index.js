e05c71554c129111ad29e13b008ad2b4ff5be66a
jyotibis <jyotibisht7683@gmail.com>
2023-04-06 10:07:28 +0530

    error resolved

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "https://chatapplicationdev.netlify.app/",
+        origin: "https://chatapplicationdev.netlify.app",
         methods: ["GET", "POST"],
     },
 });
